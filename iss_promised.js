345c272fe9ee10122ae25b9552babebf6157a17a
Georgina Bada <georginabada@gmail.com>
2021-12-02 22:49:44 -0500

    ISS Spotter With Promises

---
+++ iss_promised.js
@@ -0,0 +1,12 @@
+
+const nextISSTimesForMyLocation = function() {
+  return fetchMyIP()
+    .then(fetchCoordsByIP)
+    .then(fetchISSFlyOverTimes)
+    .then((data) => {
+      const { response } = JSON.parse(data);
+      return response;
+    });
+};
+
+module.exports = { nextISSTimesForMyLocation };
